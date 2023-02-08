871f06c54d50e47a12643f9d15ee138ad392f507
samuelberhane <1samibrhane@gmail.com>
2023-02-08 09:21:45 +0300

    Make Navbar component responsive.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -26,44 +26,50 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="bg-gray-800 flex justify-between px-12  text-white items-center sticky top-0 h-20 z-50">
+    <div className="bg-gray-800 flex justify-between px-2 md:px-6 lg:px-10 text-white items-center sticky top-0 h-20 z-50">
       {/* logo */}
-      <Link href="/" className="flex text-3xl items-center">
+      <Link
+        href="/"
+        className="md:text-2xl lg:text-3xl items-center hidden md:flex"
+      >
         Daily <DiJqueryLogo className="text-blue-300" /> Meal
       </Link>
 
       {/* links */}
-      <ul className="flex gap-4 items-center text-lg">
+      <ul className="flex gap-[0.35rem] md:gap-2 lg:gap-4 items-center text-sm md:text-[16px] lg:text-lg">
         <li className="flex items-center relative text-blue-300">
           <Link href="/">
             <DiJqueryLogo className="absolute -top-2 -left-2 " /> Home
           </Link>
         </li>
         <li>
-          {" "}
-          <Link href="#reviews">Reviews</Link>{" "}
+          <Link href="#reviews">Reviews</Link>
         </li>
         <li>
-          <Link href="#about">About Us</Link>{" "}
+          <Link href="#about" className="whitespace-nowrap">
+            About Us
+          </Link>
         </li>
         <li>
-          {" "}
-          <Link href="#join">Join Us</Link>{" "}
+          <Link href="#join" className="whitespace-nowrap">
+            Join Us
+          </Link>
         </li>
         <li>
-          {" "}
-          <Link href="#contact">Contacts</Link>{" "}
+          <Link href="#contact" className="whitespace-nowrap">
+            Contacts
+          </Link>
         </li>
       </ul>
 
       {/* order, cart and phone */}
-      <div className="flex gap-8 items-center text-xl">
+      <div className="flex gap-2 md:gap-6 lg:gap-8 items-center text-sm md:text-[16px] lg:text-lg ">
         <Link href={currentOrder ? `/order/${currentOrder}` : "/"}>Orders</Link>
-        <div className="flex gap-1 items-center">
+        <div className="gap-1 items-center hidden md:flex">
           <BsFillTelephoneInboundFill className="text-2xl text-yellow-500" />
           <p>+1 876 124 243</p>
         </div>
-        <div className="relative">
+        <div className="relative mr-2 md:mr-0">
           <BsFillCartDashFill
             className="text-2xl cursor-pointer"
             onClick={() => dispatch(HANDLE_CART())}
