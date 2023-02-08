ed90c829cda262f7eee4225e8cb39b6e6148dd35
samuelberhane <1samibrhane@gmail.com>
2023-02-08 10:56:15 +0300

    Make Joinus component responsive.

--- components/Joinus.jsx
+++ components/Joinus.jsx
@@ -1,21 +1,23 @@
 const Joinus = () => {
   return (
     <div className="mt-8" id="join">
-      <h1 className="text-center font-bold text-4xl">
-        JOIN OUR EAT WELL,
-        <span className="text-yellow-700">BE WELL CULTURE.</span>
+      <h1 className="text-center font-bold text-3xl">
+        Join Our Eat Well,
+        <span className="text-yellow-700">Be Well Culture.</span>
       </h1>
-      <p className="text-center my-3 ">
+      <p className="text-center my-3">
         Stay up to date with our openings,upcoming events,seasonal specials, and
         promotions.
       </p>
-      <div className="flex items-center justify-center mt-6 ">
+      <div className="flex items-center justify-center mt-6">
         <input
           type="text"
           placeholder="Enter Your Email"
-          className="outline-none border-2 px-4 py-2 w-[50%] bg-gray-300"
+          className="outline-none border-2 px-2 py-2 w-[70%] md:w-[60%] lg:w-[50%] bg-gray-300"
         />
-        <button className="bg-black text-white h-11 px-4">SIGN ME UP!</button>
+        <button className="bg-black text-white h-11 px-2 text-sm">
+          SIGN ME UP!
+        </button>
       </div>
     </div>
   );
