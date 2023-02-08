0690be79d497a84416a1338963226a6067e1907b
samuelberhane <1samibrhane@gmail.com>
2023-02-08 09:51:12 +0300

    Responsive HomeTop component.

--- components/HomeTop.jsx
+++ components/HomeTop.jsx
@@ -5,29 +5,23 @@ import { TbTruckDelivery } from "react-icons/tb";
 
 const HomeTop = () => {
   return (
-    <div className="home relative flex justify-end ">
-      <div className="foodImg pt-8">
-        <Image
-          src="https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1600"
-          alt="food"
-          width="700"
-          height="400"
-          className="rounded-xl"
-        />
-      </div>
-      <div className="absolute deliveryText z-20 w-96">
-        <h1 className="text-6xl font-bold">
-          Fastest <span className="text-yellow-600">Delivery</span> & Easy{" "}
-          <span className="text-yellow-600">Pickup</span>
+    <div className="home grid lg:grid-cols-2 gap-4 pt-5 px-4 md:px-10 lg:px-14">
+      <div className="z-20">
+        <h1 className="text-4xl md:text-6xl font-bold text-white">Fastest</h1>
+        <h1 className="text-4xl text-yellow-600 md:text-6xl font-bold">
+          Delivery <span className="text-white">&</span>
         </h1>
-        <p className="my-8 text-xl">Get It Delivered Right To Your Door.</p>
-        <div className="w-full  relative">
+        <h1 className="text-4xl text-yellow-600 md:text-6xl font-bold mt-1">
+          Easy <span className="text-white">Pickup</span>
+        </h1>
+        <p className="my-6 text-xl">Get It Delivered Right To Your Door.</p>
+        <div className="w-full relative pr-4 mb-4 max-w-[500px]">
           <input
             type="text"
-            placeholder="Search Food Or Restaurat"
+            placeholder="Search Food Or Restaurant"
             className="bg-white text-black  rounded-2xl px-3 py-2 outline-none w-full"
           />
-          <p className="absolute right-0 top-1 z-10 bg-yellow-600  px-5 py-1 rounded-2xl">
+          <p className="absolute right-5 top-1 z-10 bg-yellow-600  px-5 py-1 rounded-2xl">
             Search
           </p>
         </div>
@@ -42,6 +36,14 @@ const HomeTop = () => {
             <TbTruckDelivery /> <p>FAST DELIVERY</p>
           </div>
         </div>
+      </div>
+      <div className="hidden lg:inline h-[95%] w-full relative pb-12">
+        <Image
+          src="https://images.pexels.com/photos/1640772/pexels-photo-1640772.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt="food"
+          layout="fill"
+          className="rounded-xl"
+        />
       </div>
     </div>
   );
